2cece027342bca14bfe3f043adc5205f2207b6be
Artem <vinogradovartem0412@gmail.com>
2023-11-10 04:23:29 +0300

    test

---
+++ django_web/app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
+from main.views import IndexPageView, ChangeLanguageView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', IndexPageView.as_view(), name='index'),
+]
